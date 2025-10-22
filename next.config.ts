58f86bc879354f76aeaeb0c7d234802969e3578b
Kenya Okubo <ken82265@berkeley.edu>
2025-10-23 08:54:27 +0900

    add supabase setting

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,10 @@ const nextConfig: NextConfig = {
     protocol: 'https',
     hostname: 'picsum.photos',
     },
+    {
+      protocol: 'https',
+      hostname: 'puozuusdagbjealwgadx.supabase.co'
+    }
     ],
     unoptimized: false,
     domains: ['localhost'],
